e7759627cf9f376906dbb8ea3370f4fb671b6e6b
egolife <egor.zubarev@gmail.com>
2014-04-05 15:51:02 +0400

    Se

--- scripts/main.js
+++ scripts/main.js
@@ -24,5 +24,114 @@ $(document).ready(function(){
 		extendSearch.css("margin-left", "640px").hide(0).css("width", 300).slideDown(200);
 		specialityForm.html(devSpec);
 	});
+	$("#metro_select").click(function(e){
+		$("#freelance_container").html("");
+	});
 	extendSearch.leanModal({overlay : 0.9, top: 200});
+	//slider(0,300,'review-slider', true, 1, true);
 });
+
+
+
+function slider(currentPosition, slideWidth, slideBlock, showStatusPosition, step, setinterval){
+    /*
+    * example html code
+    *
+    * <div id="slideBlock">
+    *     <div class="control" control="left">back</div>
+    *     <div class="slidesContainer">
+    *       <div class="slide">1</div>
+    *       <div class="slide">2</div>
+    *       <div class="slide">3</div>
+    *     </div>
+    *     <div class="control" control="right">next</div>
+    * </div>
+    *
+    * slider(0,300,'slideBlock');  <-- JS
+    * */
+    var currentPosition = currentPosition; // Start position
+    var slideWidth = slideWidth;        // width one slide
+    var slidesBlock = $('#'+slideBlock+'');  // id block our slider
+    var slidesContainer = slidesBlock.find('.slidesContainer');
+    var slides = slidesContainer.find('.slide');
+    var numberOfSlides = slides.length;
+    var showStatusPosition = showStatusPosition;
+	var step = step;  /*    step = 1 elemet*/
+	var setinterval = setinterval;
+	var idInterval;
+	var timeout = 5000;
+	
+    slidesContainer.css('overflow', 'hidden');
+    slides
+        .wrapAll('<div class="slideInner"></div>')
+        .css({
+            'float' : 'left',
+            'width' : slideWidth
+        });
+
+    slidesContainer.find('.slideInner').css('width', slideWidth * numberOfSlides);
+
+    if(showStatusPosition) slidesBlock.find('.statusPosition ul[class="controlPosition"] li:nth-child('+(currentPosition+1)+')').addClass('active');
+
+    slidesBlock.find('.control')
+        .bind('click', function(){
+            currentPosition = ($(this).attr('control') == 'right') ? currentPosition + step : currentPosition - step;
+            if(currentPosition < 0) currentPosition = numberOfSlides - step;
+            if(currentPosition == numberOfSlides) currentPosition = 0;
+            setupStatusPosition(currentPosition);
+            slidesContainer.find('.slideInner').animate({
+                'marginLeft' : slideWidth * (- currentPosition)
+            });
+        });
+
+	if(setinterval){
+		    idInterval = setInterval( function() { 
+			currentPosition = currentPosition + step;
+            if(currentPosition < 0) currentPosition = numberOfSlides - step;
+            if(currentPosition == numberOfSlides) currentPosition = 0;
+            setupStatusPosition(currentPosition);
+            slidesContainer.find('.slideInner').animate({
+                'marginLeft' : slideWidth * (- currentPosition)
+            }); 
+		} , timeout);
+		
+		slidesBlock.hover(
+			function(){
+			  clearInterval(idInterval)
+			},
+			function(){
+				idInterval = setInterval( function() { 
+					currentPosition = currentPosition + step;
+					if(currentPosition < 0) currentPosition = numberOfSlides - step;
+					if(currentPosition == numberOfSlides) currentPosition = 0;
+					setupStatusPosition(currentPosition);
+					slidesContainer.find('.slideInner').animate({
+						'marginLeft' : slideWidth * (- currentPosition)
+					}); 
+				} , timeout);
+			}
+		);
+    }
+	
+    if(showStatusPosition){
+        slidesBlock.find('.statusPosition ul[class="controlPosition"] li')
+            .bind('click', function(){
+                currentPosition = $(this).index();
+                slidesContainer.find('.slideInner').animate({
+                    'marginLeft' : slideWidth * (- currentPosition)
+                });
+                setupStatusPosition(currentPosition);
+            });
+
+    }
+
+    function setupStatusPosition(currentPosition){
+        if(showStatusPosition){
+            var position = currentPosition + 1;
+            slidesBlock.find('.statusPosition ul[class="controlPosition"] li').each(function(){ $(this).removeClass('active');});
+            slidesBlock.find('.statusPosition ul[class="controlPosition"] li:nth-child('+position+')').addClass('active');
+        }
+    }
+	
+	
+}
